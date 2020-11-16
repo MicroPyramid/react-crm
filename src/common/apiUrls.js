6c85dabdd59b6c024c78c9219fdb888de5f61232
mahesh <m3540397@gmail.com>
2020-11-16 16:08:22 +0530

    fix: authentication api urls

--- src/common/apiUrls.js
+++ src/common/apiUrls.js
@@ -1,2 +1,2 @@
-export const DOMAIN = 'https://bottlecrm.com/api-common/';
-export const ACCOUNTS = 'https://bottlecrm.com/api-accounts/';
+export const DOMAIN = 'https://bottlecrm.com/api/auth/';
+export const ACCOUNTS = 'https://bottlecrm.com/api/accounts/';
