cf01dd98ce133ea551f2220fddeacd88d45a9395
mahesh <m3540397@gmail.com>
2020-11-28 10:39:41 +0530

    update ActionButtons UI Component on leads

--- src/crm/Leads/Leads.js
+++ src/crm/Leads/Leads.js
@@ -1,5 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {timeFromNow} from '../Utilities';
+import ViewActionButton from '../UIComponents/ActionButtons/ViewActionButton';
+import EditActionButton from '../UIComponents/ActionButtons/EditActionButton';
+import DeleteActionButton from '../UIComponents/ActionButtons/DeleteActionButton';
+
+
 export default function Leads(props) {
 
   const [openLeads, setOpenLeads] = useState([]);
@@ -67,30 +72,20 @@ export default function Leads(props) {
                                 </span>
                               )
                             })
-                            : 'No Tags'                  
-                        } 
+                            : 'No Tags'
+                        }
                       </div>
                     </td>
                     <td>{(lead.country) ? lead.country: 'None'}</td>
                     <td title="Nov. 27, 2020, 4:08 p.m.">{timeFromNow(lead.created_on)}</td>
-                    <td className="actions action-flex">
-                      <a href={`/accounts/${lead.id}/view/`} className="action__btn-email-a mr-1" title="View" data-account={lead.id} data-account-email={lead.email}  title="Send Mail"
-                          data-toggle="modal" data-target="#send_email_to_contacts">
-                        <svg className="action__btn action__btn-email svg-inline--fa fa-paper-plane fa-w-16" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="paper-plane" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"></path></svg>
-                      </a>
-                      <a href={`/accounts/${lead.id}/view/`} className="action__btn-view-a mr-1" title="View">
-                        <svg className="action__btn action__btn-view  svg-inline--fa fa-eye fa-w-18 svg-size" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="eye" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"></path></svg>              
-                      </a>
-                      <a href={`/accounts/${lead.id}/edit/`} className="action__btn-edit-a edit mr-1" title="Edit">
-                        <svg className="action__btn action__btn-edit svg-inline--fa fa-pencil-alt fa-w-16 svg-size" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pencil-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"></path></svg>
-                      </a>
-                      <a href="/accounts/" className="action__btn-delete-a delete remove_account" title="Delete">
-                        <svg className="action__btn action__btn-delete svg-inline--fa fa-trash-alt fa-w-14 svg-size" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>
-                      </a>
+                    <td className="actions action-flex">                        
+                        <ViewActionButton lead={lead} to="leads"/>
+                        <EditActionButton lead={lead} to="leads"/>
+                        <DeleteActionButton lead={lead} to="leads"/>                      
                     </td>        
                   </tr>)
              })
-           }                       
+           }
         </tbody>
       </table>
     )    
